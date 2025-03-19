a9b9180b47b9d0a311af170ebba5d64b12af3090
owenmartin-bd <owen0918@gmail.com>
2025-03-19 18:21:15 +0600

    Maps completed

--- 15-maps/tutorial.go
+++ 15-maps/tutorial.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var mp map[string]int = map[string]int{
+		"apple":  5,
+		"pear":   6,
+		"orange": 9,
+	}
+
+	// mp := make(map[string]int)
+	// fmt.Println(mp["apples"])
+
+	// mp["Owen"] = 900 // Add a new key-value pair
+
+	// delete(mp, "apple") // Delete a key-value pair
+
+	val, ok := mp["apple"]
+
+	fmt.Println(val, ok)
+
+	fmt.Println(mp)
 }
